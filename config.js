21fe6e27be333f4b77701a7eff8a83c1b46dda99
annihayahinfo <annihayahinfo@gmail.com>
2026-09-11 11:50:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // KONFIGURASI FRONTEND
 // GANTI URL DI BAWAH dengan URL /exec hasil Deploy Google Apps Script Anda
 // ============================================================
-const GAS_URL = 'https://script.google.com/macros/s/GANTI_DENGAN_DEPLOYMENT_ID/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycby5HxUc9xoPe9miG2iUxvqcFDcbeiwkYUsXlVIhex4zMexP_NxoKwVroEWDOQYCgDWxgA/exec';
